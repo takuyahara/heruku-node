e7adeccdab5c23c4c2ce797e665115651cd8cae3
takuyahara <h.taku86@gmail.com>
2019-03-30 01:24:36 +0900

    test

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const server = app.listen(3000, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log("Node.js is listening to PORT:" + server.address().port);
 });
 app.get('/api/sonarcloud', function(req, res) {
